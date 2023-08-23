087a66accf9bd698d651b5609bfad42cfca0cd69
test <fluxlinkage@sina.com>
2023-08-23 14:08:03 +0800

    增加PyTorch Lightning 2.0支持

--- RWKV-v4neo/src/model.py
+++ RWKV-v4neo/src/model.py
@@ -708,9 +708,10 @@ class RWKV(pl.LightningModule):
         return L2Wrap.apply(loss, logits)
 
     def training_step_end(self, batch_parts):
-        all = self.all_gather(batch_parts)
-        if self.trainer.is_global_zero:
-            self.trainer.my_loss_all = all
+        if pl.__version__[0]!='2':
+            all = self.all_gather(batch_parts)
+            if self.trainer.is_global_zero:
+                self.trainer.my_loss_all = all
 
     def generate_init_weight(self):
         print(
