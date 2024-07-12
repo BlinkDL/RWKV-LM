d4583e696340fde120fbef0ca0c0e612bb2b26a8
Felix Lin <linxz02@gmail.com>
2024-07-11 23:41:13 -0400

    up

--- RWKV-v5/template/submit.sh
+++ RWKV-v5/template/submit.sh
@@ -1,10 +1,9 @@
 rm -f slurm.err slurm.log
 
-export RUN_NAME=04b-pre-x52   # used by: both slurm (squeue) & wandb. (cf train.py)
+export RUN_NAME=1b5-tunefull-x58   # used by: both slurm (squeue) & wandb. (cf train.py)
 
 sbatch --job-name=$RUN_NAME slurm-rva.sh
 
-sbatch slurm-rva.sh
 echo "wait for 1 sec..."; sleep 1
 # https://stackoverflow.com/questions/42217102/expand-columns-to-see-full-jobname-in-slurm
 #squeue --format="%.18i %.20P %.15j %.8u %.8T %R" --me
