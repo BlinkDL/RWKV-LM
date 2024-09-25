a55c70ce36b564f24f8a085979dc056cf8a1c5f0
fxlin <linxz02@gmail.com>
2024-09-24 21:04:18 -0400

    up

---
+++ RWKV-v5/env-pi.sh
@@ -0,0 +1,17 @@
+#module load nvtop
+#module load cuda-toolkit-12.4.0   # needed. 
+#alias python='python3'
+#alias pip3='python3 -m pip'
+
+#export CUDA_VISIBLE_DEVICES=1,2,3
+#export CUDA_VISIBLE_DEVICES=0
+
+source ${HOME}/workspace-rwkv/myenv/bin/activate
+
+#expected by deepspeed installation 
+#export CUDA_HOME=/sw/ubuntu-22.04/cuda/12.4.0/
+#export CUDA_HOME=/usr/local/cuda-12/
+# many things will install here
+#export PATH=$PATH:${HOME}/.local/bin
+
+export RWKVDATA=/data/rwkv-data
