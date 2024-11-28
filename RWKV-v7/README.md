218a94cf373a04bd906e7a234134227ac03c2641
Bo <a@a.com>
2024-11-28 19:20:46 +0800

    rwkv-7 release ("rc4a")

--- RWKV-v7/README.md
+++ RWKV-v7/README.md
@@ -1,7 +1,7 @@
-# RWKV-7 "Goose" x070.rc4-2411 (WIP, final release soon)
+# RWKV-7 "Goose" x070 (final release, "rc4a")
 
 **Train RWKV-7**: use /RWKV-v5/ and use --my_testing "x070" in demo-training-prepare.sh and demo-training-run.sh
 
 Please try https://github.com/BlinkDL/RWKV-LM/blob/main/RWKV-v7/rwkv_v7_demo.py
 
-Please check https://github.com/BlinkDL/modded-nanogpt-rwkv for RWKV-7 efficient fwd & bwd CUDA kernel.
+Please check https://github.com/BlinkDL/modded-nanogpt-rwkv for more RWKV-7 efficient fwd & bwd CUDA kernels.
--- RWKV-v7/rwkv_v7_demo.py
+++ RWKV-v7/rwkv_v7_demo.py
@@ -9,13 +9,13 @@ from torch.nn import functional as F
 np.set_printoptions(precision=4, suppress=True, linewidth=200)
 
 '''
-This will load RWKV-7 "Goose" x070.rc4a-2411 and inference in GPT-mode (slower than RNN-mode for autoregressive generation)
+This will load RWKV-7 "Goose" x070 and inference in GPT-mode (slower than RNN-mode for autoregressive generation)
 '''
 
 args = types.SimpleNamespace()
 
-# model download: https://huggingface.co/BlinkDL/temp-latest-training-models/tree/main
-MODEL_PATH = "/mnt/e/rwkv-x070-rc4a-172m-pile-20241120-ctx4k.pth"
+# model download: https://huggingface.co/BlinkDL/rwkv-7-pile
+MODEL_PATH = "/mnt/e/RWKV-x070-Pile-168M-20241120-ctx4096.pth"
 
 args.n_layer = 12
 args.n_embd = 768
@@ -26,9 +26,7 @@ tokenizer = Tokenizer.from_file("../RWKV-v4neo/20B_tokenizer.json")
 # DTYPE = torch.bfloat16
 DTYPE = torch.half # better
 
-RESCALE_LAYER = -1 # not used here
 args.head_size_a = 64 # don't change
-args.head_size_divisor = 8 # don't change
 HEAD_SIZE = args.head_size_a
 
 USE_CUDA_KERNEL = True # False => UNOPTIMIZED, VERY SLOW
@@ -111,78 +109,79 @@ class RWKV_Tmix_x070(nn.Module):
         self.n_head = args.dim_att // self.head_size
         assert args.dim_att % self.n_head == 0
 
-        with torch.no_grad():
-            ddd = torch.empty(1, 1, args.n_embd)
-            self.time_maa_r = nn.Parameter(ddd)
-            self.time_maa_w = nn.Parameter(ddd)
-            self.time_maa_k = nn.Parameter(ddd)
-            self.time_maa_v = nn.Parameter(ddd)
-            self.time_maa_a = nn.Parameter(ddd)
-            self.time_maa_g = nn.Parameter(ddd)
+        H = self.n_head
+        N = self.head_size
+        C = args.n_embd
 
-            self.time_decay = nn.Parameter(torch.empty(1,1,args.dim_att))
-            self.time_faaaa = nn.Parameter(torch.empty(self.n_head,self.head_size))
-            self.time_aaaaa = nn.Parameter(torch.empty(1,1,args.dim_att))
+        self.xx_r = nn.Parameter(torch.empty(1,1,C))
+        self.xx_w = nn.Parameter(torch.empty(1,1,C))
+        self.xx_k = nn.Parameter(torch.empty(1,1,C))
+        self.xx_v = nn.Parameter(torch.empty(1,1,C))
+        self.xx_a = nn.Parameter(torch.empty(1,1,C))
+        self.xx_g = nn.Parameter(torch.empty(1,1,C))
 
-            D_DECAY_LORA = 64
-            self.time_decay_w1 = nn.Parameter(torch.empty(args.n_embd, D_DECAY_LORA))
-            self.time_decay_w2 = nn.Parameter(torch.empty(D_DECAY_LORA, args.dim_att))
+        D_DECAY_LORA = 64
+        self.ww_b = nn.Parameter(torch.empty(1,1,C))
+        self.ww_w1 = nn.Parameter(torch.empty(C, D_DECAY_LORA))
+        self.ww_w2 = nn.Parameter(torch.empty(D_DECAY_LORA, C))
 
-            D_AAA_LORA = 64
-            self.time_aaa_w1 = nn.Parameter(torch.empty(args.n_embd, D_AAA_LORA))
-            self.time_aaa_w2 = nn.Parameter(torch.empty(D_AAA_LORA, args.dim_att))
+        D_AAA_LORA = 64
+        self.aa_b = nn.Parameter(torch.empty(1,1,C))
+        self.aa_w1 = nn.Parameter(torch.empty(C, D_AAA_LORA))
+        self.aa_w2 = nn.Parameter(torch.empty(D_AAA_LORA, C))
 
-            D_GATE_LORA = 128
-            self.gate_w1 = nn.Parameter(torch.empty(args.n_embd, D_GATE_LORA))
-            self.gate_w2 = nn.Parameter(torch.empty(D_GATE_LORA, args.dim_att))
+        if layer_id > 0:
+            D_MV_LORA = 32
+            self.vv_b = nn.Parameter(torch.empty(1,1,C))
+            self.vv_w1 = nn.Parameter(torch.empty(C, D_MV_LORA))
+            self.vv_w2 = nn.Parameter(torch.empty(D_MV_LORA, C))
 
-            if layer_id > 0:
-                D_MV_LORA = 32
-                self.mv_w1 = nn.Parameter(torch.empty(args.n_embd, D_MV_LORA))
-                self.mv_w2 = nn.Parameter(torch.empty(D_MV_LORA, args.dim_att))
-                self.time_misc_v = nn.Parameter(torch.empty(1,1,args.n_embd))
+        D_GATE_LORA = 128
+        self.gg_w1 = nn.Parameter(torch.empty(C, D_GATE_LORA))
+        self.gg_w2 = nn.Parameter(torch.empty(D_GATE_LORA, C))
 
-            self.time_misc_kkk = nn.Parameter(torch.empty(1,1,args.n_embd))
-            self.time_misc_a = nn.Parameter(torch.empty(1,1,args.n_embd))
+        self.kk_s = nn.Parameter(torch.empty(1,1,C))
+        self.ka_s = nn.Parameter(torch.empty(1,1,C))
+        self.rk_s = nn.Parameter(torch.empty(H,N))
 
-            self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
-            self.receptance = nn.Linear(args.n_embd, args.dim_att, bias=False)
-            self.key = nn.Linear(args.n_embd, args.dim_att, bias=False)
-            self.value = nn.Linear(args.n_embd, args.dim_att, bias=False)
-            self.output = nn.Linear(args.dim_att, args.n_embd, bias=False)
-            self.ln_x = nn.GroupNorm(self.n_head, args.dim_att, eps=(1e-5)*(args.head_size_divisor**2))
+        self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
+        self.receptance = nn.Linear(C, C, bias=False)
+        self.key = nn.Linear(C, C, bias=False)
+        self.value = nn.Linear(C, C, bias=False)
+        self.output = nn.Linear(C, C, bias=False)
+        self.ln_x = nn.GroupNorm(H, C, eps=64e-5) # !!! notice eps value !!!
 
     def forward(self, x, v0):
         B, T, C = x.size()
         H = self.n_head
         xx = self.time_shift(x) - x
-        xr = x + xx * self.time_maa_r
-        xw = x + xx * self.time_maa_w
-        xk = x + xx * self.time_maa_k
-        xv = x + xx * self.time_maa_v
-        xa = x + xx * self.time_maa_a
-        xg = x + xx * self.time_maa_g
+        xr = x + xx * self.xx_r
+        xw = x + xx * self.xx_w
+        xk = x + xx * self.xx_k
+        xv = x + xx * self.xx_v
+        xa = x + xx * self.xx_a
+        xg = x + xx * self.xx_g
 
         r = self.receptance(xr)
-        w = -F.softplus(-(self.time_decay + torch.tanh(xw @ self.time_decay_w1) @ self.time_decay_w2)) - 0.5 # soft-clamp to (-inf, -0.5)
+        w = -F.softplus(-(self.ww_b + torch.tanh(xw @ self.ww_w1) @ self.ww_w2)) - 0.5 # soft-clamp to (-inf, -0.5)
         k = self.key(xk)
         v = self.value(xv)
         if self.layer_id == 0:
             v0 = v
         else:
-            v = v + (v0 - v) * torch.sigmoid(self.time_misc_v + (xv @ self.mv_w1) @ self.mv_w2)
-        a = torch.sigmoid(self.time_aaaaa + (xa @ self.time_aaa_w1) @ self.time_aaa_w2) # a is "in-context learning rate"
-        g = torch.sigmoid(xg @ self.gate_w1) @ self.gate_w2
+            v = v + (v0 - v) * torch.sigmoid(self.vv_b + (xv @ self.vv_w1) @ self.vv_w2)
+        a = torch.sigmoid(self.aa_b + (xa @ self.aa_w1) @ self.aa_w2) # a is "in-context learning rate"
+        g = torch.sigmoid(xg @ self.gg_w1) @ self.gg_w2
 
-        kk = k * self.time_misc_kkk
+        kk = k * self.kk_s
         kk = F.normalize(kk.view(B,T,H,-1), dim=-1, p=2.0).view(B,T,C)
-        k = k * (1 + (a-1) * self.time_misc_a)
+        k = k * (1 + (a-1) * self.ka_s)
 
         x = RWKV7_OP(r, w, k, v, -kk, kk*a)
 
         x = self.ln_x(x.view(B * T, C)).view(B, T, C)
         
-        x = x + ((r.view(B,T,H,-1)*k.view(B,T,H,-1)*self.time_faaaa).sum(dim=-1, keepdim=True) * v.view(B,T,H,-1)).view(B,T,C)
+        x = x + ((r.view(B,T,H,-1)*k.view(B,T,H,-1)*self.rk_s).sum(dim=-1, keepdim=True) * v.view(B,T,H,-1)).view(B,T,C)
 
         x = self.output(x * g)
         return x, v0
@@ -199,15 +198,15 @@ class RWKV_CMix_x070(nn.Module):
         self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
 
         with torch.no_grad():
-            self.time_maa_k = nn.Parameter(torch.empty(1, 1, args.n_embd))
+            self.xx_k = nn.Parameter(torch.empty(1, 1, args.n_embd))
 
-        self.key = nn.Linear(args.n_embd, args.n_embd * 4, bias=False)
-        self.value = nn.Linear(args.n_embd * 4, args.n_embd, bias=False)
+        self.key = nn.Linear(args.n_embd, args.dim_ffn, bias=False)
+        self.value = nn.Linear(args.dim_ffn, args.n_embd, bias=False)
 
     def forward(self, x):
         xx = self.time_shift(x) - x
         
-        k = x + xx * self.time_maa_k
+        k = x + xx * self.xx_k
         k = torch.relu(self.key(k)) ** 2
         return self.value(k)
 
@@ -239,12 +238,6 @@ class Block(nn.Module):
         x = x + xx
         x = x + self.ffn(self.ln2(x))
 
-        # if RESCALE_LAYER > 0:
-        #     if (self.layer_id+1) % RESCALE_LAYER == 0:
-        #         x = x / 2
-        # if self.layer_id == args.n_layer-1:
-        #     print(torch.min(x).item(), torch.max(x).item())
-
         return x, v0
 
 ########################################################################################################
@@ -255,7 +248,7 @@ class RWKV(nn.Module):
     def __init__(self, args):
         super().__init__()
         args.dim_att = args.n_embd
-        args.dim_ffn = int((args.n_embd * 3.5) // 32 * 32)
+        args.dim_ffn = args.n_embd * 4
         self.emb = nn.Embedding(args.vocab_size, args.n_embd)
 
         self.blocks = nn.ModuleList([Block(args, i) for i in range(args.n_layer)])
@@ -281,17 +274,6 @@ class RWKV(nn.Module):
 ########################################################################################################
 
 model_params = torch.load(MODEL_PATH, map_location="cpu")
-keys = list(model_params.keys())
-for k in keys:
-    layer_id = int(k.split('.')[1]) if ('blocks.' in k) else 0
-    
-    if '.time_faaaa' in k: model_params[k] = model_params[k].reshape(-1, args.head_size_a)
-    
-    # if RESCALE_LAYER > 0:
-    #     if 'att.output.weight' in k:
-    #         model_params[k] = model_params[k] / (2 ** int(layer_id // RESCALE_LAYER))
-    #     if 'ffn.value.weight' in k:
-    #         model_params[k] = model_params[k] / (2 ** int(layer_id // RESCALE_LAYER))
 
 with torch.no_grad():
 
@@ -307,7 +289,7 @@ with torch.no_grad():
     out = model.forward(torch.tensor(input).reshape(1,-1).cuda())
     print(f'\nOutput:\n{out}')
 
-    # let's check the logits for the last token => prediction for the next token    
+    # logits of the last token => prediction for the next token    
     out = out[0, -1]
     
     probs = F.softmax(out.float(), dim=-1) # compute softmax in float (more accurate)
