852cd8fb8006a4b0c30e795634995672933c59be
PENG Bo <33809201+BlinkDL@users.noreply.github.com>
2024-12-13 08:45:18 +0800

    Update wkv6state_cuda.cu

--- RWKV-v5/cuda/wkv6state_cuda.cu
+++ RWKV-v5/cuda/wkv6state_cuda.cu
@@ -12,7 +12,7 @@ __global__ void kernel_forward(const int B, const int T, const int C, const int 
     const int h = blockIdx.x % H;
     const int i = threadIdx.x;
     _u += h*_N_;
-    _s += h*_N_*_N_ + i*_N_;
+    _s += b*H*_N_*_N_ + h*_N_*_N_ + i*_N_;
 
     __shared__ float r[_N_], k[_N_], u[_N_], w[_N_];
     float state[_N_];
