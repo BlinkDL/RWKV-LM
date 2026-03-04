178a93904895b855c65e1d5ef47139a96c3e3002
PENG Bo <33809201+BlinkDL@users.noreply.github.com>
2026-03-05 06:42:33 +0800

    Update demo-training-run.sh

--- RWKV-v7/train_temp/demo-training-run.sh
+++ RWKV-v7/train_temp/demo-training-run.sh
@@ -47,7 +47,7 @@ EPOCH_SAVE=10 # save every 10 "miniepochs" (1 miniepoch = 40320 * ctx_len tokens
 N_NODE=1 # number of nodes
 GPU_PER_NODE=1 # number of GPUs per node
 #
-DS_BUCKET_MB=2 # set to 2 for consumer GPUs, set to 200 for A100 / H100 (affects speed & vram usage)
+# DS_BUCKET_MB=2 # set to 2 for consumer GPUs, set to 200 for A100 / H100 (affects speed & vram usage) UPDATE: very buggy in new deepspeed, so I disabled it
 #
 python train.py --load_model "0" --wandb "Test" --proj_dir $PROJ_DIR --my_testing $MODEL_TYPE \
  --ctx_len $CTX_LEN --train_stage 3 --epoch_count 999999 --epoch_begin 0 \
@@ -55,4 +55,4 @@ python train.py --load_model "0" --wandb "Test" --proj_dir $PROJ_DIR --my_testin
  --num_nodes $N_NODE --micro_bsz $M_BSZ --n_layer $N_LAYER --n_embd $N_EMBD \
  --lr_init $LR_INIT --lr_final $LR_FINAL --warmup_steps 10 --beta1 0.9 --beta2 0.99 --adam_eps 1e-18 --data_type "binidx" --vocab_size 65536 \
  --weight_decay 0.001 --epoch_save $EPOCH_SAVE --head_size 64 \
- --accelerator gpu --devices $GPU_PER_NODE --precision bf16 --strategy deepspeed_stage_2 --grad_cp $GRAD_CP --enable_progress_bar True --ds_bucket_mb $DS_BUCKET_MB
+ --accelerator gpu --devices $GPU_PER_NODE --precision bf16 --strategy deepspeed_stage_2 --grad_cp $GRAD_CP --enable_progress_bar True #--ds_bucket_mb $DS_BUCKET_MB
